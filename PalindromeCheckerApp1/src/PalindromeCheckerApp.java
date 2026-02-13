0189f5d983b6b89270300423ca6bedeb1969c720
mahathivaka <mv9148@srmist.edu.in>
2026-02-13 16:09:45 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeCheckerApp1/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+            System.out.println(
+                    "Welcome to the Palindrome Checker Management System\n Version:1 \n System initialized successfully."
+            );
+    }
+}
